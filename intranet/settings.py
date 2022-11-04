a16e8880b2fc059c8ba6ad96403fb1243367275f
gabrielahlert <gabrielahlert@hotmail.com>
2022-11-04 11:19:23 +0000

    Add requeriments.txt and python-decouple

--- intranet/settings.py
+++ intranet/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,16 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 #SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-z+ac-!dk!)+8jal2y3)!t8efu2riml141tvy^k%bg2ipv5$x4$'
-#with open( 'C:/secret_key.txt') as f:
-#    SECRET_KEY = f.read().strip()
-SECRET_KEY = 'Gim6pWfCBuDk@3C&hEfbBTu5zvH@&HXYfs6Vm5^CCkvPY6$bJ#Dtqy$#&GepWcmUXmPRzYTZGb6bN#VF^6Xjmgs78A$L5W^#b9pqJJTbWePbXsnqcT%r7ygx*5Jhy$&i'
+SECRET_KEY = config('SECRET_KEY', default='Gim6pWfCBuDk@3C&hEfbBTu5zvH@&HXYfs6Vm5^CCkvPY6$bJ#Dtqy$#&GepWcmUXmPRzYTZGb6bN#VF^6Xjmgs78A$L5W^#b9pqJJTbWePbXsnqcT%r7ygx*5Jhy$&i')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -80,23 +77,25 @@ WSGI_APPLICATION = 'intranet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_intranet',
-        'USER': 'db_intranet_user',
-        'PASSWORD': 'passdbintranet',
-        'HOST': 'localhost',
-        'PORT': '',
+if config('DB', default='sqlite') == 'sqlite':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+elif config('DB') == 'postgresql':
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 
