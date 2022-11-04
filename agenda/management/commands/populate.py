56f37075ac56f5172640a7fd49351db95aff17f3
Gabriel Ahlert <50123942+gabribr10@users.noreply.github.com>
2022-11-04 17:11:06 -0300

    Update populate.py

--- agenda/management/commands/populate.py
+++ agenda/management/commands/populate.py
@@ -23,6 +23,7 @@ class Command(BaseCommand):
 
                     if nascimento == None or nascimento == '':
                         pessoa_ativo = False
+                        nascimento = None
                     else:
                         nascimento = datetime.strptime(nascimento, '%d/%m/%Y').date()
 
@@ -34,6 +35,10 @@ class Command(BaseCommand):
                     
                     funcao, _ = Funcao.objects.get_or_create(nome=funcao)
                     unidade, _ = Unidade.objects.get_or_create(nome=unidade)
-                    Contato.objects.update_or_create(nome=nome.title(), ramal=ramal, telefone=telefone, email=email, funcao=funcao, unidade=unidade, nascimento=nascimento, ramal_ativo=ramal_ativo, pessoa_ativo=pessoa_ativo)
+                    obj, created =  Contato.objects.update_or_create(nome=nome.title(), defaults={'nome':nome.title(), 'ramal':ramal, 'telefone':telefone, 'email':email, 'funcao':funcao, 'unidade':unidade, 'nascimento':nascimento, 'ramal_ativo':ramal_ativo, 'pessoa_ativo':pessoa_ativo})
+                    if created:
+                        self.stdout.write(self.style.SUCCESS('Successfully created "%s"' % obj))
+                    else:
+                        self.stdout.write(self.style.SUCCESS('Successfully updated "%s"' % obj))
 
         self.stdout.write(self.style.SUCCESS('Successfully populated the database'))
