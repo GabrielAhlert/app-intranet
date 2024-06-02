4388d4e8733c1324df762e5f43ae870bae1a3afe
Gabriel Ahlert <gabriel.ahlert@hotmail.com>
2024-06-02 19:00:16 -0300

    Add Events JS

--- home/static/home/calendar_script.js
+++ home/static/home/calendar_script.js
@@ -1,6 +1,6 @@
-const daysTag = document.querySelector(".days"),
-currentDate = document.querySelector(".current-date"),
-prevNextIcon = document.querySelectorAll(".icons span");
+const daysTag = document.querySelector(".days"), 
+    currentDate = document.querySelector(".current-date"),
+    prevNextIcon = document.querySelectorAll(".icons span");
 
 let date = new Date(),
 currYear = date.getFullYear(),
@@ -9,41 +9,67 @@ currMonth = date.getMonth();
 const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
 const renderCalendar = () => {
-let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(),
-lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
-lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(),
-lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
-let liTag = "";
+    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(),
+    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
+    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(),
+    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+    let liTag = "";
 
-for (let i = firstDayofMonth; i > 0; i--) {
-liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
-}
-
-for (let i = 1; i <= lastDateofMonth; i++) { let isToday=i===date.getDate() && currMonth===new Date().getMonth() && currYear===new Date().getFullYear() ? "active" : "" ; liTag +=`<li class="${isToday}">${i}</li>`;
+    for (let i = firstDayofMonth; i > 0; i--) {
+        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
     }
 
-    for (let i = lastDayofMonth; i < 6; i++) { liTag +=`<li class="inactive">${i - lastDayofMonth + 1}</li>`
-        }
-        currentDate.innerText = `${months[currMonth]} ${currYear}`;
-        daysTag.innerHTML = liTag;
-        }
-        renderCalendar();
+    for (let i = 1; i <= lastDateofMonth; i++) {
+        let isToday = i === date.getDate() && currMonth === new Date().getMonth() && currYear === new Date().getFullYear() ? "today" : ""; liTag += `<li class="${isToday}" id="${i}">${i}</li>`;
+    }
 
-        prevNextIcon.forEach(icon => {
-        icon.addEventListener("click", () => { 
-            if (icon.id === "today") {
+    for (let i = lastDayofMonth; i < 6; i++) {
+        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
+    }
+    currentDate.innerText = `${months[currMonth]} ${currYear}`;
+    daysTag.innerHTML = liTag;
+
+    let days = document.querySelectorAll(".days li");
+
+    days.forEach(day => {
+        day.addEventListener("click", () => {
+            console.log(day.id);
+            if (day.classList.contains("inactive")) return;
+            try {
+                document.querySelector(".active").classList.remove("active");
+            } catch (error) {
+                console.log("No active class");
+            }
+            day.classList.add("active");
+            loadEvents(new Date(currYear, currMonth, day.id));
+        });
+    });
+}
+renderCalendar();
+
+prevNextIcon.forEach(icon => {
+    icon.addEventListener("click", () => {
+        if (icon.id === "today") {
             currYear = date.getFullYear();
             currMonth = date.getMonth();
-            }else
+        } else
             currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
 
-        if(currMonth < 0 || currMonth> 11) {
+        if (currMonth < 0 || currMonth > 11) {
             date = new Date(currYear, currMonth);
             currYear = date.getFullYear();
             currMonth = date.getMonth();
-            } else {
+        } else {
             date = new Date();
-            }
-            renderCalendar();
-            });
-            });
+        }
+        renderCalendar();
+    });
+});
+
+async function loadEvents(date){
+    console.log(date);
+    const h1 = document.getElementById("h1-eventos");
+    h1.innerHTML = "Eventos do dia " + date.toLocaleDateString();
+}
+
+
