024c059354dbad7bf4cfcd1183717772479a50fa
Ruan Vasconcelos <ruanvasconcelos898@gmail.com>
2024-06-11 17:57:32 -0300

    recados refeitos

--- recados/models.py
+++ recados/models.py
@@ -1,18 +1,20 @@
 from django.db import models
-from ckeditor_uploader.fields import RichTextUploadingField
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 
 class Recado(models.Model):
     titulo = models.CharField(max_length=64, blank=True)
     # texto = models.TextField(blank=True)
-    texto = RichTextUploadingField(blank=True)
+    texto = RichTextField(blank=True)
     data_inicio = models.DateField()
     data_fim = models.DateField()
     ativo = models.BooleanField(default=True)
-    # image = models.ImageField(upload_to='images/', blank=True, null=True)
 
     def __str__(self):
-        return self.titulo
-    
+        return self.titulo    
+
+class ImagemRecado(models.Model):
+    recado = models.ForeignKey(Recado, on_delete=models.CASCADE, blank=True, null=True)
+    image = models.ImageField(upload_to='images/')
 
