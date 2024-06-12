e90e978fbe615025cc809586a4dcf523080f5c35
Ruan Vasconcelos <ruanvasconcelos898@gmail.com>
2024-06-12 17:43:22 -0300

    permitido enviar midias no campo texto do recado

--- recados/models.py
+++ recados/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
 
 class Recado(models.Model):
     titulo = models.CharField(max_length=64, blank=True)
     # texto = models.TextField(blank=True)
-    texto = RichTextField(blank=True)
+    texto = RichTextUploadingField()
     data_inicio = models.DateField()
     data_fim = models.DateField()
     ativo = models.BooleanField(default=True)
