75114ee9775f2304a8f40cee691da939f159f9c9
Ruan Vasconcelos <ruanvasconcelos898@gmail.com>
2024-06-13 14:42:49 -0300

    allow null in eventos.local

--- home/models.py
+++ home/models.py
@@ -12,7 +12,7 @@ class Banner(models.Model):
     
 class Evento(models.Model):
     titulo = models.CharField(max_length=100, default='Evento '+str(timezone.now()))
-    local = models.CharField(max_length=100, default='Local '+str(timezone.now()))
+    local = models.CharField(max_length=100, blank=True, null=True,  default='Local '+str(timezone.now()))
     data = models.DateField(default=timezone.now)
 
     def __str__(self):
