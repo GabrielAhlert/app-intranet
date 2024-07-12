84fa700b1613eeee951b7107691ee2a229fc91f8
Gabriel Ahlert <50123942+GabrielAhlert@users.noreply.github.com>
2024-07-12 18:00:51 -0300

    Update views.py

--- base/views.py
+++ base/views.py
@@ -13,10 +13,16 @@ def refresh_token():
     body = { {"email": config("COTACAO_USER"), "password": config("COTACAO_PASS")}}
     token = requests.post(url + 'admin/login', data=json.dumps(body), headers=headers).json()['data']['token']
     os.environ["COTRISOJA_API_TOKEN"] = token
+    
+def getToken():
+    token = os.environ.get("COTRISOJA_API_TOKEN")
+    if token:
+        return token
+    return "InvalidTokenRefresh"
 
 def get_cotacao(request):
     try:
-        headers = { 'Authorization' : 'Bearer ' + os.environ.get("COTRISOJA_API_TOKEN"), 'Content-Type': 'application/json'}
+        headers = { 'Authorization' : 'Bearer ' +  getToken(), 'Content-Type': 'application/json'}
         url = 'https://sp-app.cotrisoja.com.br/api/'
         url_cotacao = url + requests.get(url + 'dashboard', headers=headers).json()['agricultural_quotation']['formats']['large']['url']
         return HttpResponseRedirect(url_cotacao)
@@ -28,4 +34,5 @@ def get_cotacao(request):
             except:
                 return HttpResponseBadRequest('Erro ao buscar cotação')
                 pass
+    
     
