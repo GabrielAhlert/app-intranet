528c5692e41f678967febd0315bfb09ea9151c6f
Gabriel Ahlert <50123942+GabrielAhlert@users.noreply.github.com>
2024-07-12 17:42:46 -0300

    Update views.py

--- base/views.py
+++ base/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 import requests
-from decouple import config
 from django.http import HttpResponseRedirect, HttpResponse as Http
 from django.http import HttpResponseBadRequest
+from decouple import config
+import os
+from requests.exceptions import RequestException as HttpRequestError
+import json
+
+def refresh_token():
+    headers = { 'Content-Type': 'application/json'}
+    url = config("COTACAO_URL")
+    body = { {"email": config("COTACAO_USER"), "password": config("COTACAO_PASS")}}
+    token = requests.post(url + 'admin/login', data=json.dumps(body), headers=headers).json()['data']['token']
+    os.environ["COTRISOJA_API_TOKEN"] = token
 
 def get_cotacao(request):
     try:
-        headers = { 'Authorization' : 'Bearer ' + config('COTRISOJA_API_TOKEN'), 'Content-Type': 'application/json'}
+        headers = { 'Authorization' : 'Bearer ' + os.environ["COTRISOJA_API_TOKEN"], 'Content-Type': 'application/json'}
         url = 'https://sp-app.cotrisoja.com.br/api/'
         url_cotacao = url + requests.get(url + 'dashboard', headers=headers).json()['agricultural_quotation']['formats']['large']['url']
         return HttpResponseRedirect(url_cotacao)
-    except:
-        return HttpResponseBadRequest('Erro ao buscar cotação')
+    except HttpRequestError as e:
+        if(e.response.status_code == 401):
+            try:
+                refresh_token()
+                return get_cotacao(request)
+            except:
+                return HttpResponseBadRequest('Erro ao buscar cotação')
+                pass
+    
