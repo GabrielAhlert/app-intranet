d78c1e87554ec8fd8e2d8bd34750ea43daa9dd1c
Gabriel Ahlert <50123942+GabrielAhlert@users.noreply.github.com>
2024-07-12 18:05:44 -0300

    Update views.py

--- base/views.py
+++ base/views.py
@@ -8,17 +8,22 @@ from requests.exceptions import RequestException as HttpRequestError
 import json
 
 def refresh_token():
-    headers = { 'Content-Type': 'application/json'}
-    url = config("COTACAO_URL")
-    body = { {"email": config("COTACAO_USER"), "password": config("COTACAO_PASS")}}
-    token = requests.post(url + 'admin/login', data=json.dumps(body), headers=headers).json()['data']['token']
-    os.environ["COTRISOJA_API_TOKEN"] = token
+    try:
+        headers = { 'Content-Type': 'application/json'}
+        url = config("COTACAO_URL")
+        body = { {"email": config("COTACAO_USER"), "password": config("COTACAO_PASS")}}
+        token = requests.post(url + 'admin/login', data=json.dumps(body), headers=headers).json()['data']['token']
+        os.environ["COTRISOJA_API_TOKEN"] = token
+        return token
+    except:
+        return HttpResponseBadRequest('Erro ao buscar cotação')
     
 def getToken():
     token = os.environ.get("COTRISOJA_API_TOKEN")
     if token:
         return token
-    return "InvalidTokenRefresh"
+    else:
+        return refresh_token()
 
 def get_cotacao(request):
     try:
@@ -27,11 +32,6 @@ def get_cotacao(request):
         url_cotacao = url + requests.get(url + 'dashboard', headers=headers).json()['agricultural_quotation']['formats']['large']['url']
         return HttpResponseRedirect(url_cotacao)
     except:
-        try:
-            refresh_token()
-            return get_cotacao(request)
-        except:
-            return HttpResponseBadRequest('Erro ao buscar cotação')
-            pass
+        refresh_token()
     
     
