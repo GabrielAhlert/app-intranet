94831cc62776b21eeb4ef5b65652478cf97c664e
Ruan Vasconcelos <ruanvasconcelos898@gmail.com>
2025-02-18 14:07:28 -0300

    fixed contact search

--- base/static/base/filter.js
+++ base/static/base/filter.js
@@ -1,25 +1,25 @@
 const tablefilter = () => {
-	const trs = document.querySelectorAll('.table tr:not(.head)');
-	const filter = document.querySelector('.txtSearch').value.trim().toLowerCase();
+    const trs = document.querySelectorAll('.table tr:not(.head)');
+    const filter = document.querySelector('#search_ramal').value.trim().toLowerCase();
+    
+    if (!filter) {
+        trs.forEach(tr => tr.style.display = '');
+        return;
+    }
 
-	if (!filter) {
-		trs.forEach(tr => tr.style.display = '');
-		return;
-	}
+    const filterWords = filter.split(/\s+/); 
 
-	const filterWords = filter.split(/\s+/);
+    const isFoundInTds = td => {
+        const text = td.textContent.toLowerCase();
+        return filterWords.every(word => text.includes(word)); 
+    };
 
-	const isFoundInTds = td => {
-		const text = td.textContent.toLowerCase();
-		return filterWords.every(word => text.includes(word));
-	};
+    const isFound = childrenArr => childrenArr.some(isFoundInTds);
 
-	const isFound = childrenArr => childrenArr.some(isFoundInTds);
-
-	trs.forEach(tr => {
-		tr.style.display = isFound([...tr.children]) ? '' : 'none';
-	});
+    trs.forEach(tr => {
+        tr.style.display = isFound([...tr.children]) ? '' : 'none';
+    });
 };
 
-document.querySelector('.txtSearch').addEventListener('keyup', tablefilter);
-document.querySelector('.txtSearch').focus();
+document.querySelector('#search_ramal').addEventListener('keyup', tablefilter);
+document.querySelector('#search_ramal').focus();
